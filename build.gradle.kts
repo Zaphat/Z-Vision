bccd39fd8c4bf8ec4afb382428a269b383e6a524
Zaphat <93189118+Zaphat@users.noreply.github.com>
2025-06-20 18:53:13 +0700

    improve pipeline speed

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.0" // Add Detekt plugin
+}
+
+detekt {
+    toolVersion = "1.23.0"
+    config = files("$rootDir/detekt-config.yml") // Optional: Specify a custom config file
+    buildUponDefaultConfig = true
+    allRules = false
 }
