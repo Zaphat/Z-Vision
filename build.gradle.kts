e8eb7e81740002047a329240f4d16041fd7ef54b
Zaphat <93189118+Zaphat@users.noreply.github.com>
2025-06-20 18:59:26 +0700

    ktlint fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.0" // Add Detekt plugin
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2" // Add ktlint plugin
 }
 
 detekt {
@@ -10,4 +11,10 @@ detekt {
     config = files("$rootDir/detekt-config.yml") // Optional: Specify a custom config file
     buildUponDefaultConfig = true
     allRules = false
+}
+
+ktlint {
+    version.set("0.48.2") // Specify ktlint version
+    enableExperimentalRules.set(true) // Enable experimental rules if needed
+    verbose.set(true) // Enable verbose output
 }
